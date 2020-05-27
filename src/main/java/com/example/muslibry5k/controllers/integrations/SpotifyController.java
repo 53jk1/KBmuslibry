7a8d0eced6257d4315c21d9fc688ab4550604681
Sejki <bonczalke@gmail.com>
2020-05-27 19:05:12 +0200

    M6.3

--- src/main/java/com/example/muslibry5k/controllers/integrations/SpotifyController.java
+++ src/main/java/com/example/muslibry5k/controllers/integrations/SpotifyController.java
@@ -21,10 +21,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-
-/**
- * Created by pk on 2019-06-01
- */
 @Controller
 public class SpotifyController {
 
